25441e56e7972e3f945cf443fd51e0546aec1076
Sajidul Kabir <sajidulkabiradib@gmail.com>
2022-07-05 13:54:38 +0600

    local development config added

--- db.js
+++ db.js
@@ -4,16 +4,16 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 let pool = mysql.createPool({
-  //   host: "localhost",
-  //   user: process.env.DB_USERNAME,
-  //   database: process.env.DB,
-  //   waitForConnections: true,
-  //   connectionLimit: 10,
-  host: "db",
-  port: "3306",
-  user: "root",
-  password: "root",
-  database: "OtterTech",
+  host: "localhost",
+  user: process.env.DB_USERNAME,
+  database: process.env.DB,
+  waitForConnections: true,
+  connectionLimit: 10,
+  // host: "db",
+  // port: "3306",
+  // user: "root",
+  // password: "root",
+  // database: "OtterTech",
 });
 console.log("database connected");
 pool = pool.promise();
