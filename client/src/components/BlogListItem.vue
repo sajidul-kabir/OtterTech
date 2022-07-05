f237f262feff98ed9bfbe4f469fc3e97e34977e6
Sajidul Kabir <sajidulkabiradib@gmail.com>
2022-07-05 22:29:50 +0600

    initial blog list rendering added

---
+++ client/src/components/BlogListItem.vue
@@ -0,0 +1,72 @@
+<template>
+  <li class="section-2-post">
+    <div class="section-2-post-container">
+      <img src="" alt="cover-image" />
+      <div class="section-2-post-container-text">
+        <h2>
+          {{ title }}
+        </h2>
+        <p>{{ blog }}</p>
+        <p>By {{ user }}, 2 hours ago</p>
+      </div>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['id', 'title', 'blog', 'user'],
+};
+</script>
+
+<style scoped>
+.section-2-post {
+  list-style: none;
+  padding: 40px 0px;
+  border-bottom: 1px solid #dedede;
+  padding-bottom: 50px;
+}
+.section-2-post-container {
+  display: flex;
+  align-items: center;
+}
+.section-2-post-container img {
+  opacity: 1;
+  max-width: 300px;
+  height: auto;
+  transition: 0.5s ease;
+}
+.section-2-post-container img:hover {
+  opacity: 0.7;
+  cursor: pointer;
+}
+.section-2-post-container h2,
+p {
+  margin: 0;
+}
+.section-2-post-container-text {
+  display: flex;
+  flex-direction: column;
+  margin-left: 25px;
+  margin-bottom: 50px;
+}
+.section-2-post-container-text h2 {
+  font-size: 26px;
+  max-width: 450px;
+  color: #2b282c;
+}
+.section-2-post-container-text h2:hover {
+  cursor: pointer;
+  text-decoration: underline 1px solid rgba(0, 0, 0, 0.603);
+  text-underline-position: under;
+}
+.section-2-post-container-text p {
+  margin-top: 10px;
+  max-width: 450px;
+  color: #000000d1;
+  width: 100ch;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+</style>
--- client/src/pages/home/HomePage.vue
+++ client/src/pages/home/HomePage.vue
@@ -1,24 +1,54 @@
 <template>
-  <h1>HOME</h1>
+  <blog-list :blogs="blogs"></blog-list>
 </template>
 
 <script>
 import axios from 'axios';
+import BlogList from '../../components/BlogList.vue';
+
 axios.defaults.withCredentials = true;
 
 export default {
-  mounted() {
+  beforeCreate() {
     axios
       .get('http://localhost:5000/api/users/me')
       .then((res) => {
-        console.log(res.data.data[0].username);
         if (res.data.message === 'NOT LOGGED IN') {
           this.$router.push('/login');
+        } else {
+          console.log(res.data.data[0].username);
         }
       })
       .catch((err) => {
         console.log(err);
       });
   },
+  mounted() {
+    axios
+      .get('http://localhost:5000/api/blogs')
+      .then((res) => {
+        this.blogs = res.data.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+  data() {
+    return {
+      blogs: [],
+    };
+  },
+  components: { BlogList },
 };
 </script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Inter', sans-serif;
+  font-family: 'Lora', serif;
+  margin: 0;
+}
+</style>
