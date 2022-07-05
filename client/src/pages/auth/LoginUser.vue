f049470ca536294b25cb47b27e610eceb126464d
Sajidul Kabir <sajidulkabiradib@gmail.com>
2022-07-05 17:55:07 +0600

    login through client side implement

--- client/src/pages/auth/LoginUser.vue
+++ client/src/pages/auth/LoginUser.vue
@@ -1,10 +1,51 @@
 <template>
   <div>
-    <form>
-      <input type="text" name="username" id="username" placeholder="username" />
-      <input type="password" name="password" id="pass" placeholder="password" />
+    <form @submit.prevent="loginUser">
+      <input
+        type="text"
+        name="username"
+        id="username"
+        placeholder="username"
+        v-model="user.username"
+      />
+      <input
+        type="password"
+        name="password"
+        id="pass"
+        placeholder="password"
+        v-model="user.password"
+      />
       <button>Login</button>
     </form>
     <p><router-link to="/register">Register</router-link> instead</p>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      user: {
+        username: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    loginUser() {
+      axios
+        .post('http://localhost:5000/api/users/login', this.user)
+        .then((res) => {
+          console.log(res);
+        })
+        .then(() => {
+          this.$router.push('/');
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
