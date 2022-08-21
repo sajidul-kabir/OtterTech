ad23a703612ae89216c280cd4ebdb48f40891716
Sajidul Kabir <sajidulkabiradib@gmail.com>
2022-08-21 19:55:46 +0600

    change and delete photo added

--- client/src/pages/profile/MyProfile.vue
+++ client/src/pages/profile/MyProfile.vue
@@ -10,6 +10,16 @@
         <v-avatar color="grey lighten-1" size="128">
           <v-icon color="black" size="80"> mdi-account </v-icon>
         </v-avatar>
+        <div class="my-2">
+          <v-btn color="#2b282c" large class="profile-change-picture pt-4 pb-7"
+            >Change Picture
+          </v-btn>
+        </div>
+        <div class="my-2">
+          <v-btn class="profile-delete-picture pt-3 pb-7">
+            Delete Picture
+          </v-btn>
+        </div>
       </div>
     </div>
   </div>
@@ -32,5 +42,14 @@ export default {
   width: 35%;
 }
 .public-profile {
+}
+.profile-change-picture {
+  color: white;
+  font-size: 12px;
+}
+.profile-delete-picture {
+  color: red;
+  font-size: 12px;
+  border: 1px solid red;
 }
 </style>
